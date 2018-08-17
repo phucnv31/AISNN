071164d34e1f4c12230fb22e516ed41134e3f824
PHUC\nguyenvan <nvphuc2@cmc.com.vn>
2018-08-17 12:44:36 +0700

    commit

--- AISIW/ShowImage.Designer.cs
+++ AISIW/ShowImage.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.listViewImage = new System.Windows.Forms.ListView();
+            this.labelIndex = new System.Windows.Forms.Label();
+            this.buttonTrain = new System.Windows.Forms.Button();
+            this.labelTrain = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,18 +43,91 @@
             // 
             this.pictureBox.Location = new System.Drawing.Point(0, 1);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(565, 405);
+            this.pictureBox.Size = new System.Drawing.Size(1034, 690);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
             this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
             this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
             // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(1158, 526);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(44, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(1055, 526);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(43, 23);
+            this.buttonPrev.TabIndex = 2;
+            this.buttonPrev.Text = "Prev";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(1107, 569);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(44, 23);
+            this.buttonOk.TabIndex = 3;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // listViewImage
+            // 
+            this.listViewImage.Location = new System.Drawing.Point(1040, 1);
+            this.listViewImage.Name = "listViewImage";
+            this.listViewImage.Size = new System.Drawing.Size(172, 519);
+            this.listViewImage.TabIndex = 4;
+            this.listViewImage.UseCompatibleStateImageBehavior = false;
+            this.listViewImage.View = System.Windows.Forms.View.List;
+            this.listViewImage.SelectedIndexChanged += new System.EventHandler(this.listViewImage_SelectedIndexChanged);
+            // 
+            // labelIndex
+            // 
+            this.labelIndex.AutoSize = true;
+            this.labelIndex.Location = new System.Drawing.Point(1113, 531);
+            this.labelIndex.Name = "labelIndex";
+            this.labelIndex.Size = new System.Drawing.Size(0, 13);
+            this.labelIndex.TabIndex = 5;
+            // 
+            // buttonTrain
+            // 
+            this.buttonTrain.Location = new System.Drawing.Point(1107, 608);
+            this.buttonTrain.Name = "buttonTrain";
+            this.buttonTrain.Size = new System.Drawing.Size(44, 23);
+            this.buttonTrain.TabIndex = 6;
+            this.buttonTrain.Text = "Train";
+            this.buttonTrain.UseVisualStyleBackColor = true;
+            this.buttonTrain.Click += new System.EventHandler(this.buttonTrain_Click);
+            // 
+            // labelTrain
+            // 
+            this.labelTrain.AutoSize = true;
+            this.labelTrain.Location = new System.Drawing.Point(1113, 644);
+            this.labelTrain.Name = "labelTrain";
+            this.labelTrain.Size = new System.Drawing.Size(0, 13);
+            this.labelTrain.TabIndex = 7;
+            // 
             // ShowImage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 404);
+            this.ClientSize = new System.Drawing.Size(1215, 693);
+            this.Controls.Add(this.labelTrain);
+            this.Controls.Add(this.buttonTrain);
+            this.Controls.Add(this.labelIndex);
+            this.Controls.Add(this.listViewImage);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonNext);
             this.Controls.Add(this.pictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -57,11 +137,19 @@
             this.Load += new System.EventHandler(this.ShowImage_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.ListView listViewImage;
+        private System.Windows.Forms.Label labelIndex;
+        private System.Windows.Forms.Button buttonTrain;
+        private System.Windows.Forms.Label labelTrain;
     }
 }
--- AISIW/ShowImage.cs
+++ AISIW/ShowImage.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using ANN;
 using Accord.Imaging.Converters;
+using System.IO;
+using AForge.Imaging.Filters;
+
 namespace AISIW
 {
     public partial class ShowImage : Form
@@ -21,6 +24,12 @@ namespace AISIW
         Point LeftTop = new Point();
         Point RightBottom = new Point();
         bool isPaint = false;
+        DirectoryInfo folderImage = new DirectoryInfo("Image");
+        DirectoryInfo folderImageTmp = new DirectoryInfo("ImageTmp");
+        FileInfo[] FI;
+        Image ImageSelected;
+        int imageSelectIndex = 0;
+        Readini readIni = new Readini(@"ImageTmp\imageInfo.ini");
         public ShowImage(Bitmap bmf)
         {
             InitializeComponent();
@@ -31,6 +40,14 @@ namespace AISIW
         {
             //Stopwatch stopWatch = new Stopwatch();
             //stopWatch.Start();
+            FI = folderImage.GetFiles().Where(x => x.FullName.EndsWith(".bmp") || x.FullName.EndsWith(".png") || x.FullName.EndsWith(".jpg")).ToArray();
+            var fileNames = FI.Select(x => x.Name);
+            listViewImage.Columns.Add("Name");
+            listViewImage.Columns[0].Width = listViewImage.Width;
+            for (int i = 0; i < fileNames.Count(); i++)
+            {
+                listViewImage.Items.Add(fileNames.ToArray()[i]);
+            }
             LoadImage();
             ImageToArray conv = new ImageToArray(min: 0, max: 1);
             double[] Input;
@@ -105,11 +122,11 @@ namespace AISIW
         {
             //string fileNameBM = @"Image\BM.bmp";
             //Bitmap bm = new Bitmap(Image.FromFile(fileNameBM));
-            Width = bm.Width;
-            Height = bm.Height;
-            BackgroundImage = bm;
-            pictureBox.Width = bm.Width;
-            pictureBox.Height = bm.Height;
+            //Width = bm.Width;
+            //Height = bm.Height;
+            //BackgroundImage = bm;
+            //pictureBox.Width = bm.Width;
+            //pictureBox.Height = bm.Height;
         }
         public void DrawRectangle(Point leftTop, Point rightBottom)
         {
@@ -117,26 +134,27 @@ namespace AISIW
             {
                 var width = Math.Abs(rightBottom.X - leftTop.X);
                 var height = Math.Abs(rightBottom.Y - leftTop.Y);
-                //Pen p = new Pen(Color.Red, 2);
-                //Graphics paper = this.CreateGraphics();
-                //paper.DrawRectangle(p, leftTop.X, leftTop.Y, width, height);
-                //p.Dispose();
-                //paper.Dispose();
-                Bitmap bitmap = new Bitmap(this.bm);
-                // Draw the rectangle.
-                using (Graphics gr = Graphics.FromImage(bitmap))
+                if (pictureBox.Image != null)
                 {
-                    gr.DrawRectangle(Pens.Red, leftTop.X, leftTop.Y, width, height);
+                    Bitmap bm = new Bitmap(ImageSelected, ImageSelected.Width, ImageSelected.Height);
+                    using (Graphics gr = Graphics.FromImage(bm))
+                    {
+                        gr.DrawRectangle(Pens.Red, leftTop.X, leftTop.Y, width, height);
+                    }
+                    pictureBox.Image = bm;
+                    this.Invalidate();
                 }
-                pictureBox.Image = bitmap;
             }
         }
         public void Draw()
         {
-            LeftTop.X = FirstPoint.X <= FinalPoint.X ? FirstPoint.X : FinalPoint.X;
-            LeftTop.Y = FirstPoint.Y <= FinalPoint.Y ? FirstPoint.Y : FinalPoint.Y;
-            RightBottom.X = FirstPoint.X >= FinalPoint.X ? FirstPoint.X : FinalPoint.X;
-            RightBottom.Y = FirstPoint.Y >= FinalPoint.Y ? FirstPoint.Y : FinalPoint.Y;
+            if (ImageSelected == null)
+                return;
+            LeftTop = new Point(Math.Min(FirstPoint.X, FinalPoint.X), Math.Min(FirstPoint.Y, FinalPoint.Y));
+            RightBottom = new Point(Math.Max(FirstPoint.X, FinalPoint.X), Math.Max(FirstPoint.Y, FinalPoint.Y));
+            LeftTop = new Point(Math.Max(pictureBox.Location.X, LeftTop.X), Math.Max(pictureBox.Location.Y, LeftTop.Y));
+            Point maxRightBottom = new Point(pictureBox.Location.X + ImageSelected.Width, pictureBox.Location.Y + ImageSelected.Height);
+            RightBottom = new Point(Math.Min(maxRightBottom.X, RightBottom.X), Math.Min(maxRightBottom.Y, RightBottom.Y));
             DrawRectangle(LeftTop, RightBottom);
         }
 
@@ -161,6 +179,129 @@ namespace AISIW
                 FinalPoint = e.Location;
                 Draw();
             }
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            var P = ConvertLocationToP(LeftTop, RightBottom, new Point(0, 0), new Point(ImageSelected.Width, ImageSelected.Height));
+            string value = string.Join("|", P);
+            readIni.WriteValue("ImageInfo", FI[imageSelectIndex].Name, value);
+            if (!File.Exists(folderImageTmp.FullName + @"\" + FI[imageSelectIndex].Name))
+            {
+                ImageSelected.Save(folderImageTmp.FullName + @"\" + FI[imageSelectIndex].Name);
+            }
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (listViewImage.SelectedIndices.Count > 0)
+            {
+                DecreaseIndex();
+                SelectIndexChange(imageSelectIndex);
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (listViewImage.SelectedIndices.Count > 0)
+            {
+                IncreaseIndex();
+                SelectIndexChange(imageSelectIndex);
+            }
+        }
+
+        private void listViewImage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewImage.SelectedIndices.Count > 0)
+            {
+                imageSelectIndex = listViewImage.SelectedIndices[0];
+                SelectIndexChange(listViewImage.SelectedIndices[0]);
+                labelIndex.Text = (imageSelectIndex + 1).ToString() + "/" + FI.Count();
+            }
+        }
+        void SelectIndexChange(int selectIndex)
+        {
+            if (listViewImage.SelectedIndices.Count > 0)
+            {
+                var fileSelect = FI.ElementAtOrDefault(listViewImage.SelectedIndices[0]);
+                if (fileSelect != null)
+                {
+                    ImageSelected = Image.FromFile(fileSelect.FullName);
+                    if (ImageSelected.Width > pictureBox.Width || ImageSelected.Height > pictureBox.Height)
+                    {
+                        ResizeBilinear filter = new ResizeBilinear(pictureBox.Width, pictureBox.Height);
+                        // apply the filter
+                        Bitmap newImage = filter.Apply(new Bitmap(ImageSelected));
+                        ImageSelected = newImage;
+                    }
+                    pictureBox.Image = ImageSelected;
+                }
+            }
+        }
+        void IncreaseIndex()
+        {
+            listViewImage.Items[imageSelectIndex].Selected = false;
+            imageSelectIndex = imageSelectIndex + 1 < FI.Count() ? imageSelectIndex + 1 : 0;
+            listViewImage.Items[imageSelectIndex].Selected = true;
+            listViewImage.Select();
+            labelIndex.Text = (imageSelectIndex + 1).ToString() + "/" + FI.Count();
+        }
+        void DecreaseIndex()
+        {
+            listViewImage.Items[imageSelectIndex].Selected = false;
+            imageSelectIndex = imageSelectIndex > 0 ? imageSelectIndex - 1 : FI.Count() - 1;
+            listViewImage.Items[imageSelectIndex].Selected = true;
+            listViewImage.Select();
+            labelIndex.Text = (imageSelectIndex + 1).ToString() + "/" + FI.Count();
+        }
+        double[] ConvertLocationToP(Point leftTop, Point rightBottom, Point imageLocation, Point imageRightBottom)
+        {
+            var width = rightBottom.X - leftTop.X;
+            var height = rightBottom.Y - leftTop.Y;
+            var widthImage = imageRightBottom.X - imageLocation.X;
+            var heightImage = imageRightBottom.Y - imageLocation.Y;
+            Point middlePoint = new Point(leftTop.X + (width / 2), leftTop.Y + (height / 2));
+            double[] P = new double[5];
+            P[0] = 1;
+            P[1] = middlePoint.X * 1.0 / imageRightBottom.X;
+            P[2] = middlePoint.Y * 1.0 / imageRightBottom.Y;
+            P[3] = width * 1.0 / widthImage;
+            P[4] = height * 1.0 / heightImage;
+            return P;
+        }
+
+        private void buttonTrain_Click(object sender, EventArgs e)
+        {
+            FileInfo[] fis = folderImageTmp.GetFiles().Where(x => x.FullName.EndsWith(".bmp") || x.FullName.EndsWith(".jpg") || x.FullName.EndsWith(".png")).ToArray();
+            TrainImage(fis[1]);
+        }
+        void TrainImage(FileInfo fi)
+        {
+            string value = readIni.ReadValue("ImageInfo", fi.Name);
+            string[] P = value.Split('|');
+            double[] output = new double[P.Count()];
+            for (int i = 0; i < P.Count(); i++)
+            {
+                output[i] = double.Parse(P[i]);
+            }
+            Bitmap bitmap = new Bitmap(Image.FromFile(fi.FullName));
+            ImageToArray conv = new ImageToArray(min: 0, max: 1);
+            double[] input;
+            conv.Convert(bitmap, out input);
+            labelTrain.Text = "0/10";
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                NeuralNetwork network = new NeuralNetwork(0.00125, new int[] { bitmap.Width * bitmap.Height, 6, 5, 5 });
+                for (int i = 0; i < 10; i++)
+                {
+                    network.Train(input.ToList(), output.ToList());
+                    this.Invoke(new Action(
+                        delegate ()
+                        {
+                            labelTrain.Text = (i+1).ToString() + "/" + 10;
+                        }));
+                }
+            });
         }
     }
 }
